c127238bf3a12686316a081d5b8139698bb95f50
giacomomagni <giac.magni@gmail.com>
2022-06-06 19:07:21 +0200

    make loader working

--- src/nnusf/loader.py
+++ src/nnusf/loader.py
@@ -13,17 +13,21 @@ class Loader:
         path to commondata folder
     data_name:
         dataset name
+    data_type: str
+        data type: F2,F3, DXDYNUU, DXDYNUB
 
     """
 
-    def __init__(self, path_to_commondata: pathlib.Path, data_name: str) -> None:
+    def __init__(
+        self, path_to_commondata: pathlib.Path, data_name: str, data_type: str
+    ) -> None:
         self.path = path_to_commondata.joinpath("commondata")
-        self.data_name = data_name
+        self.data_name = f"{data_name}_{data_type}"
 
         self.kin_df, self.data_df, unc_df = self.load()
         self.covariance_matrix = self.build_covariance_matrix(unc_df)
 
-    def load(self) -> tuple(pd.DataFrame):
+    def load(self) -> tuple:
         """Load the dataset information
 
         Returns
@@ -36,37 +40,33 @@ class Loader:
             uncertainties
 
         """
-
         # info file
-        info_df = pd.read_csv(f"{self.path}/info/{self.data_name}.csv")
-        data_type = info_df["type"]
+        exp_name = self.data_name.split("_")[0]
+        info_df = pd.read_csv(f"{self.path}/info/{exp_name}.csv")
 
         # kinematic file
-        kin_file = self.path.joinpath(
-            f"kinematics/KIN_{self.data_name}_{data_type}.csv"
-        )
-        if kin_file.exist():
-            kin_df = pd.read_csv(
-                f"{self.path}/kinematics/KIN_{self.data_name}_{data_type}.csv"
-            )
+        kin_file = self.path.joinpath(f"kinematics/KIN_{self.data_name}.csv")
+        if kin_file.exists():
+            kin_df = pd.read_csv(kin_file)[2:].astype(float)
         else:
-            kin_df = pd.read_csv(
-                f"{self.path}/kinematics/KIN_{self.data_name}_F2F3.csv"
-            )
-        kin_df["A"] = info_df["target"]
+            kin_df = pd.read_csv(f"{self.path}/kinematics/KIN_{exp_name}_F2F3.csv")[
+                2:
+            ].astype(float)
+        kin_df["A"] = np.full(kin_df.shape[0], info_df["target"][0])
 
         # central values and uncertainties
-        data_df = pd.read_csv(f"{self.path}/data/DATA_{self.data_name}_{data_type}.csv")
-        unc_df = pd.read_csv(
-            f"{self.path}/uncertainties/UNC_{self.data_name}_{data_type}.csv"
+        data_df = pd.read_csv(
+            f"{self.path}/data/DATA_{self.data_name}.csv", header=1, dtype=float
         )
-
+        unc_df = pd.read_csv(f"{self.path}/uncertainties/UNC_{self.data_name}.csv")[
+            3:
+        ].astype(float)
         return kin_df, data_df, unc_df
 
     @property
-    def kinematics(self) -> tuple(np.ndarray):
+    def kinematics(self) -> tuple:
         """Returns the kinematics variables"""
-        return self.kin_df["x"], self.kin_df["q2"], self.kin_df["A"]
+        return self.kin_df["x"], self.kin_df["Q2"], self.kin_df["A"]
 
     @property
     def central_values(self) -> np.ndarray:
@@ -76,8 +76,9 @@ class Loader:
     @property
     def covmat(self) -> np.ndarray:
         """Returns the covariance matrix"""
-        return self.covariance_matrix()
+        return self.covariance_matrix
 
+    @staticmethod
     def build_covariance_matrix(unc_df: pd.DataFrame) -> np.ndarray:
         """Build the covariance matrix given the statistical and systematics uncertainties
 
@@ -91,6 +92,6 @@ class Loader:
             covariance matrix
 
         """
-        diagonal = unc_df["stat"] ** 2
+        diagonal = np.power(unc_df["stat"], 2)
         corr_sys = unc_df["syst"]
         return np.diag(diagonal) + corr_sys @ corr_sys.T
