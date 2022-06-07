7b949a5ca3edcb06de23c06205c608cf14db8f0f
Alessandro Candido <candido.ale@gmail.com>
2022-06-07 15:26:56 +0200

    Add combine subcommand for data

--- src/nnusf/cli/data.py
+++ src/nnusf/cli/data.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
-
+from ..data import combine_tables
 from . import base
 
 
 @base.command.group("data")
 def subcommand():
     """Provide data management utilities."""
+
+
+@subcommand.command("combine")
+def sub_combine():
+    """Combine tables."""
+    combine_tables.main()
