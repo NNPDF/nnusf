4a746ac82163ec47e1cf31dba8cf889ca64aa2bc
Alessandro Candido <candido.ale@gmail.com>
2022-06-07 10:51:25 +0200

    Add runcards for extra high Q2 points

--- src/nnusf/theory/highq/load.py
+++ src/nnusf/theory/highq/load.py
@@ -2,7 +2,12 @@
 import numpy as np
 import pandas as pd
 
-obsmap = {"F2": "F2_total", "F3": "F3_total", "XS": "XSCHORUSCC"}
+sfmap = {"F2": "F2_total", "FL": "FL_total", "F3": "F3_total"}
+xsmap = {"CHORUS": "XSCHORUSCC"}
+Q2CUT = 5
+
+xgrid = np.geomspace(1e-4, 1, 20)
+q2grid = np.geomspace(Q2CUT, 1e4, 20)
 
 
 def kinematics(n=1000) -> pd.DataFrame:
@@ -10,7 +15,7 @@ def kinematics(n=1000) -> pd.DataFrame:
         x=np.random.random(n),
         Q2=np.exp(np.random.random(n) * 3 + 1),
         y=np.random.random(n),
-        obs=np.random.choice(list(obsmap.keys()), size=n),
+        obs=np.random.choice(list(sfmap.keys()) + ["XS"], size=n),
         proj=np.random.randint(-1, 2, size=n),
         A=np.random.randint(1, 100, size=n),
     )
--- src/nnusf/theory/highq/runcards.py
+++ src/nnusf/theory/highq/runcards.py
@@ -19,9 +19,27 @@ def observables() -> dict:
     nb = kins["proj"] <= 0
 
     for proj, run in [(nu, run_nu), (nb, run_nb)]:
-        for obs, obsname in load.obsmap.items():
+        for obs, obsname in load.sfmap.items():
             obsfilter = kins["obs"] == obs
             obskins = list(kins[proj][obsfilter][["x", "Q2", "y"]].T.to_dict().values())
             run["observables"][obsname] = obskins
 
-    return dict(nu=run_nu, nb=run_nb)
+        #  xsfilter = kins["obs"] == "XS"
+        # TODO: recognize experiment, plug the appropriate XS for each
+        # experiment
+
+    run_nu_extra = copy.deepcopy(run_nu)
+    run_nu_extra["observables"] = {}
+
+    run_nb_extra = copy.deepcopy(run_nb)
+    run_nb_extra["observables"] = {}
+
+    for sfname in load.sfmap.values():
+        run_nu_extra["observables"][sfname] = []
+        run_nb_extra["observables"][sfname] = []
+        for x in load.xgrid:
+            for q2 in load.q2grid:
+                run_nu_extra["observables"][sfname].append(dict(x=x, Q2=q2, y=0))
+                run_nb_extra["observables"][sfname].append(dict(x=x, Q2=q2, y=0))
+
+    return dict(nu=run_nu, nb=run_nb, nu_extra=run_nb_extra, nb_extra=run_nb_extra)
