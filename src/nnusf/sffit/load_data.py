4ee46c68397f575a51901c478bebdb306533c84c
RoyStegeman <roystegeman@live.nl>
2022-06-08 14:01:17 +0200

    add make pseudodata function

--- src/nnusf/sffit/load_data.py
+++ src/nnusf/sffit/load_data.py
@@ -1,5 +1,43 @@
-from nnusf.loader import Loader
+from nnusf.data.loader import Loader
 import pathlib
+import numpy as np
+from dataclasses import dataclass
 
-path_to_commondata = pathlib.Path(__file__).parents[1].joinpath("commondata")
-path_to_theory = pathlib.Path(__file__).parents[1].joinpath("theory")
+path_to_commondata = pathlib.Path(__file__).parents[3].joinpath("commondata")
+path_to_theory = pathlib.Path(__file__).parents[3].joinpath("theory")
+
+@dataclass
+class Pseudodata:
+    "Class containing data needed to fit an experimental dataset"
+    name: str
+    tr_mask: np.ndarray
+    tr_kinematics_array: np.ndarray
+    tr_invcovmat: np.ndarray
+    tr_coefficients: np.ndarray
+    tr_central_values: np.ndarray
+    vl_kinematics_array: np.ndarray
+    vl_invcovmat: np.ndarray
+    vl_coefficients: np.ndarray
+    vl_central_values: np.ndarray
+
+
+def load_experimental_data(experiment_list):
+    "returns a dictionary with dataset names as keys and data as value"
+    experimental_data = {}
+    for experiment in experiment_list:
+        data = Loader(path_to_commondata, path_to_theory, experiment['dataset'])
+        experimental_data[experiment['dataset']] = data
+    return experimental_data
+
+
+def make_pseudodata(experimental_data_list):
+    pseudodata_dict = {}
+    for dataset_name, experimental_data in zip(experimental_data_list.keys(), experimental_data_list.values()):
+        cholesky = np.linalg.cholesky(experimental_data.covmat)
+        random_samples = np.random.randn(experimental_data.covmat.shape[0])
+        pseudodata = experimental_data.central_values + random_samples @ cholesky
+        pseudodata_dict[dataset_name] = pseudodata
+    return pseudodata_dict
+
+def make_tr_vl_objects():
+    pass
--- src/nnusf/sffit/run_sffit.py
+++ src/nnusf/sffit/run_sffit.py
@@ -5,6 +5,8 @@ Executable to perform the structure function fit
 import argparse
 import pathlib
 import yaml
+from load_data import load_experimental_data, make_pseudodata
+
 
 def main():
     parser = argparse.ArgumentParser(description="sffit - fits sfs")
@@ -17,13 +19,18 @@ def main():
     with open(path_to_runcard) as file:
         runcard_content = yaml.load(file, Loader=yaml.FullLoader)
 
+    experiments_dict = runcard_content["experiments"]
+    experimental_data = load_experimental_data(experiments_dict)
+
+    if runcard_content["pseudodataseed"]:
+        pseudo_data = make_pseudodata(experimental_data)
+
+
     # Load data
 
     # Build model
 
     # Do fit
-
-    import ipdb; ipdb.set_trace()
 
 
 if __name__ == "__main__":
