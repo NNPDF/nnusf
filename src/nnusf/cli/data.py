0051ae88dd8dfbd31d31084a365fc62a95ef05fd
Alessandro Candido <candido.ale@gmail.com>
2022-06-08 10:32:59 +0200

    Add docs for coefficients subcommand

--- src/nnusf/cli/data.py
+++ src/nnusf/cli/data.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+import pathlib
+
+import click
+
 from ..data import coefficients, combine_tables
 from . import base
 
@@ -15,10 +19,25 @@ def sub_combine():
 
 
 @subcommand.command("coefficients")
-def sub_coefficients():
+@click.argument("data", nargs=-1, type=click.Path(exists=True, path_type=pathlib.Path))
+@click.option(
+    "-d",
+    "--destination",
+    type=click.Path(path_type=pathlib.Path),
+    default=pathlib.Path.cwd().absolute() / "coefficients",
+    help="Alternative destination path to store the coefficients (default: $PWD/coefficients)",
+)
+def sub_coefficients(data, destination):
     """Provide coefficients for the observables.
 
     Dump coefficients to connect the structure functions basis (F2, FL, and F3)
     to the given experimental observable.
+
+    The operation is repeated for each DATA path provided (multiple values allowed),
+    e.g.:
+
+        nnu data coefficients commondata/data/*
+
+    to repeat the operation for all dataset stored in `data`.
     """
-    coefficients.main()
+    coefficients.main(data, destination)
--- src/nnusf/data/coefficients.py
+++ src/nnusf/data/coefficients.py
@@ -1,3 +1,11 @@
 # -*- coding: utf-8 -*-
-def main():
-    print("I'm providing them")
+import pathlib
+
+
+def main(data: list[pathlib.Path], destination: pathlib.Path):
+    destination.mkdir(parents=True, exist_ok=True)
+    print("Coefficients destination:", destination)
+
+    print("Saving coefficients:")
+    for dataset in data:
+        print("\tloaded", dataset.absolute())
