279d4f17b997712b0c57715a32ecf6ad9d94bf0c
giacomomagni <giac.magni@gmail.com>
2022-06-08 10:02:00 +0200

    black

--- src/nnusf/data/loader.py
+++ src/nnusf/data/loader.py
@@ -63,7 +63,9 @@ class Loader:
         if kin_file.exists():
             kin_df = pd.read_csv(kin_file).iloc[1:, :3]
         else:
-            kin_df = pd.read_csv(f"{self.commondata_path}/kinematics/KIN_{exp_name}_F2F3.csv").iloc[1:, :3]
+            kin_df = pd.read_csv(
+                f"{self.commondata_path}/kinematics/KIN_{exp_name}_F2F3.csv"
+            ).iloc[1:, :3]
 
         # Extract values from the central and uncertainties
         data_df = pd.read_csv(
@@ -81,7 +83,7 @@ class Loader:
         new_df = new_df.dropna().astype(float)
 
         # drop data with 0 total uncertainty:
-        new_df = new_df[ new_df['stat'] + new_df['syst'] != 0.0 ]
+        new_df = new_df[new_df["stat"] + new_df["syst"] != 0.0]
 
         number_datapoints = new_df.shape[0]
 
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -4,6 +4,7 @@ from nnusf.data.loader import Loader
 path_to_commondata = pathlib.Path(__file__).parents[1].joinpath("commondata")
 path_to_theory = pathlib.Path(__file__).parents[1].joinpath("theory")
 
+
 class TestLoader:
     def test_init(self):
         data = Loader(path_to_commondata, path_to_theory, "NUTEV", "F2")
@@ -11,3 +12,9 @@ class TestLoader:
         assert len(data.kinematics) == 3
         n_data = data.kinematics[0].shape[0]
         assert data.covmat.shape == (n_data, n_data)
+
+    def test_drop_zeros(self):
+        data = Loader(path_to_commondata, path_to_theory, "BEBCWA59", "F3")
+
+        assert len(data.kinematics) == 3
+        assert 0 not in data.fulltables["stat"] + data.fulltables["syst"]
