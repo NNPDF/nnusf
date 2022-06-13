eaf0682c4f29914f68cfd02e1eb88fd4007c32bc
Alessandro Candido <candido.ale@gmail.com>
2022-06-13 11:24:35 +0200

    Kinematic plot first candidate

--- src/nnusf/plot/kinematics.py
+++ src/nnusf/plot/kinematics.py
@@ -3,6 +3,8 @@
 import logging
 import pathlib
 
+import matplotlib.pyplot as plt
+
 from .. import utils
 from ..data import loader
 
@@ -12,10 +14,19 @@ _logger = logging.getLogger(__file__)
 def main(data: list[pathlib.Path], destination: pathlib.Path):
     """Run kinematic plot generation."""
     utils.mkdest(destination)
+
+    fig = plt.figure()
+
     for ds in data:
         name, datapath = utils.split_data_path(ds)
 
         lds = loader.Loader(name, datapath)
-        kins = {k: lds.table[k] for k in ["x", "y", "Q2"] if k in lds.table}
+        kins = [lds.table[k].values for k in ["x", "Q2"]]
+        plt.scatter(*kins, label=name, s=10)
+
+    plt.legend()
+
+    figname = destination / "kinematics.png"
+    fig.savefig(figname)
 
     _logger.info("kinematic plot")
