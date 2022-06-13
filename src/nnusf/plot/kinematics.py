7f24b835c4e81c67d2c8a0d502f724886ad118d2
Alessandro Candido <candido.ale@gmail.com>
2022-06-13 22:26:02 +0200

    Lift grouping to plot utils

--- src/nnusf/plot/kinematics.py
+++ src/nnusf/plot/kinematics.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 """Generate heatmap plots for covariance matrices."""
+import itertools
 import logging
 import pathlib
 
 import matplotlib.pyplot as plt
+import matplotlib.figure
 import numpy as np
 
 from .. import utils
@@ -13,31 +15,20 @@ from . import utils as putils
 _logger = logging.getLogger(__file__)
 
 
-def main(data: list[pathlib.Path], destination: pathlib.Path, grouping: str = "exp"):
-    """Run kinematic plot generation."""
-    utils.mkdest(destination)
+def plot(groups: dict[str, list[list[float]]]) -> matplotlib.figure.Figure:
+    """Plot (x, Q2) kinematics.
 
+    Parameters
+    ----------
+    groups: dict
+        kinematics data grouped
+    total: total amount of data points
+
+
+    """
     fig = plt.figure()
 
-    groups = {}
-    total = 0
-    for ds in data:
-        name, datapath = utils.split_data_path(ds)
-        lds = loader.Loader(name, datapath)
-        total += lds.n_data
-
-        kins = [lds.table[k].values.tolist() for k in ["x", "Q2"]]
-
-        if grouping == "exp":
-            label = lds.exp
-        else:
-            raise ValueError
-
-        if label not in groups:
-            groups[label] = [[], []]
-
-        for i in range(2):
-            groups[label][i].extend(kins[i])
+    total = sum(len(kins[0]) for kins in groups.values())
 
     for (name, kins), marker in zip(groups.items(), putils.MARKERS):
         size = len(kins[0])
@@ -46,7 +37,7 @@ def main(data: list[pathlib.Path], destination: pathlib.Path, grouping: str = "e
             label=name,
             s=100 / np.power(size, 1 / 3),
             marker=marker,
-            alpha=1 - np.tanh(2 * size / total)
+            alpha=1 - np.tanh(2 * size / total),
         )
 
     plt.yscale("log")
@@ -55,7 +46,36 @@ def main(data: list[pathlib.Path], destination: pathlib.Path, grouping: str = "e
     plt.legend()
     plt.tight_layout()
 
+    return fig
+
+
+def main(data: list[pathlib.Path], destination: pathlib.Path, grouping: str = "exp"):
+    """Run kinematic plot generation."""
+    utils.mkdest(destination)
+
+    groups = putils.group_data(
+        [loader.Loader(*utils.split_data_path(ds)) for ds in data],
+        grouping=grouping,
+    )
+
+    kingroups = {
+        name: [
+            list(
+                itertools.chain.from_iterable(
+                    lds.table[k].values.tolist() for lds in grp
+                )
+            )
+            for k in ("x", "Q2")
+        ]
+        for name, grp in groups.items()
+    }
+
+    fig = plot(kingroups)
     figname = destination / "kinematics.png"
     fig.savefig(figname)
 
-    _logger.info("kinematic plot")
+    _logger.info(
+        "Plotted [b magenta]kinematics[/] of requested datasets,"
+        f" in '{figname.relative_to(pathlib.Path.cwd())}'",
+        extra={"markup": True},
+    )
