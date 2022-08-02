01c3ccc82f5980d857e3b981a1044c846aed5335
Radonirinaunimi <rrabeman@nikhef.nl>
2022-08-02 21:10:03 +0200

    Include chi2s on real data to report

--- src/nnusf/reports/genfiles.py
+++ src/nnusf/reports/genfiles.py
@@ -18,6 +18,12 @@ def dump_to_csv(
     pdtable.to_csv(f"{output_path}/{filename}.csv")
 
 
+def json_loader(fitfolder: pathlib.Path) -> dict:
+    with open(fitfolder, "r") as fstream:
+        jsonfile = json.load(fstream)
+    return jsonfile
+
+
 def summary_table(fitfolder: pathlib.Path) -> pd.DataFrame:
     """Generate the table containing the summary of chi2s info.
 
@@ -30,14 +36,17 @@ def summary_table(fitfolder: pathlib.Path) -> pd.DataFrame:
     chi2_summary = {"tr": 0.0, "vl": 0.0}
     # Loop over the replica folder & extract chi2 info
     for count, repinfo in enumerate(fitinfos, start=1):
-        with open(repinfo, "r") as file_stream:
-            jsonfile = json.load(file_stream)
+        jsonfile = json_loader(repinfo)
         for chi2type in chi2_summary:
             chi2_summary[chi2type] += jsonfile[f"best_{chi2type}_chi2"]
+
+    # Load the json file containing experimental chi2s
+    expchi2s = json_loader(fitfolder.joinpath("expchi2s.json"))
 
     # Average the chi2 over the nb of replicas
     for chi2type in chi2_summary:
         chi2_summary[chi2type] /= count
+    chi2_summary["Exp"] = expchi2s["total_chi2"]
     summtable = pd.DataFrame.from_dict({"chi2": chi2_summary})
     dump_to_csv(fitfolder, summtable, "summary")
     return summtable
@@ -57,23 +66,27 @@ def chi2_tables(fitfolder: pathlib.Path) -> pd.DataFrame:
     datinfo = runcard_content["experiments"]
     fitinfos = fitfolder.glob("**/replica_*/fitinfo.json")
 
+    # Load the json file containing experimental chi2s
+    expchi2s = json_loader(fitfolder.joinpath("expchi2s.json"))
+
     # Initialize dictionary to store the chi2 values
     dpts_dic = {d["dataset"]: d["frac"] for d in datinfo}
     chi2_dic = {
-        d["dataset"]: {"Ndat": 0, "frac": 0, "chi2": 0.0} for d in datinfo
+        d["dataset"]: {"Ndat": 0, "frac": 0, "tr_chi2": 0.0, "exp_chi2": 0.0}
+        for d in datinfo
     }
     # Loop over the replica folder & extract chi2 info
     for count, repinfo in enumerate(fitinfos, start=1):
-        with open(repinfo, "r") as file_stream:
-            jsonfile = json.load(file_stream)
+        jsonfile = json_loader(repinfo)
         for dat in chi2_dic:
             chi2_dic[dat]["Ndat"] = jsonfile["dtpts_per_dataset"][dat]
             chi2_dic[dat]["frac"] = dpts_dic[dat]
-            chi2_dic[dat]["chi2"] += jsonfile["chi2s_per_dataset"][dat]
+            chi2_dic[dat]["tr_chi2"] += jsonfile["chi2s_per_dataset"][dat]
+            chi2_dic[dat]["exp_chi2"] = expchi2s[dat]
 
     # Average the chi2 over the nb of replicas
     for dataset_name in chi2_dic:
-        chi2_dic[dataset_name]["chi2"] /= count
+        chi2_dic[dataset_name]["tr_chi2"] /= count
 
     chi2table = pd.DataFrame.from_dict(chi2_dic, orient="index")
     dump_to_csv(fitfolder, chi2table, "chi2datasets")
