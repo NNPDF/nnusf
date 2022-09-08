fix again saving of input scaling

src/nnusf/sffit/load_fit_data.py
@@ -7,6 +7,7 @@ from typing import Union
 import numpy as np
 import tensorflow as tf
 import yaml
+from scipy.interpolate import PchipInterpolator
 
 _logger = logging.getLogger(__name__)
 
@@ -20,10 +21,12 @@ class PredictionInfo:
     predictions: Union[np.ndarray, list]
 
 
-def input_scaling(input_arr, scaler_funcs):
+def input_scaling(input_arr, map_from, map_to):
     scaled_inputs = []
     for index, kin_var in enumerate(input_arr.T):
-        input_scaling = scaler_funcs[index](kin_var)
+        input_scaling = PchipInterpolator(
+            map_from[index], map_to[index], extrapolate=True
+        )(kin_var)
         scaled_inputs.append(input_scaling)
     return np.array(scaled_inputs).T
 
@@ -64,7 +67,11 @@ def get_predictions_q(
         raise ValueError("The value of x is of an unrecognised type.")
 
     if fitcard.get("rescale_inputs", None):
-        input_list = input_scaling(np.array(input_list), fitcard["scaling"])
+        input_list = input_scaling(
+            np.array(input_list),
+            fitcard["scaling"]["map_from"],
+            fitcard["scaling"]["map_to"],
+        )
         _logger.error("Kinematic inputs are being scaled.")
 
     input_kinematics = [input_list]

src/nnusf/sffit/scaling.py
@@ -20,6 +20,7 @@ def cumulative_rescaling(
         num=sorted_kin.shape[0],
     )
 
+    mapping_from, mapping_to = [], []
     scaler_funcs = []
     for kin_var in sorted_kin.T:
         kin_unique, kin_counts = np.unique(kin_var, return_counts=True)
@@ -44,11 +45,16 @@ def cumulative_rescaling(
             map_from, map_to, extrapolate=True
         )
         scaler_funcs.append(interpolation_func)
+        mapping_to.append(map_to)
+        mapping_from.append(map_from)
 
     if save_scaling and save_scaling.is_file():
         runcard_file = yaml.load(save_scaling.read_text(), Loader=yaml.Loader)
         with open(save_scaling, "w") as ostream:
-            runcard_file["scaling"] = scaler_funcs
+            runcard_file["scaling"] = {
+                "map_from": mapping_from,
+                "map_to": mapping_to,
+            }
             yaml.dump(runcard_file, ostream, sort_keys=False)
 
     for dataset in datasets.values():