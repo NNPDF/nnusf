35af23ec2aa3761f3eb30e22999547be6009ec3d
Tanjona <rrabeman@nikhef.nl>
2022-10-31 09:32:49 +0100

    remove not yet ready module

--- src/nnusf/cli/__init__.py
+++ src/nnusf/cli/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 """Provide 'nnu' CLI."""
-from . import data, extra, fit, log, plot, report, sfpdf, theory
+from . import data, extra, fit, log, plot, report, theory
 from .base import command
